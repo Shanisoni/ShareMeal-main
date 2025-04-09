682acce7bbf3ec9997520cc47852abe488a3160d
Shanisoni <shanisoni7084@gmail.com>
2025-04-10 03:00:31 +0530

    d

--- frontend/src/components/HomeComponents/Footer.js
+++ frontend/src/components/HomeComponents/Footer.js
@@ -22,7 +22,7 @@ const Footer = () => {
             <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="social-icon" viewBox="0 0 24 24">
               <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
             </svg>
-          </a>
+          </a>   
           <a className="social-link" href="https://www.instagram.com/shani_soni__/profilecard/?igsh=YWdicjQzMm5qenJ6">
             <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="social-icon" viewBox="0 0 24 24">
               <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
