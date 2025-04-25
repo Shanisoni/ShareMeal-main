ad5c691090839cfda0bf07b98479de1893ed4fab
Shanisoni <shanisoni7084@gmail.com>
2025-04-25 18:20:06 +0530

    c

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import AppRoutes from './AppRoutes';
+
 import Header from './components/Header/Header';
+
 import Loading from './components/Loading/Loading';
+
 import { useLoading } from './hooks/useLoading';
+
+// import { setErrorInterceptor } from './interceptors/errorInterceptor';
 // import { setLoadingInterceptor } from './interceptors/loadingInterceptor';
 import { useEffect } from 'react';
 import './App.css';
