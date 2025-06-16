1cc0da03ba93141b7c395fd3404115ab282995d1
Shanisoni <shanisoni7084@gmail.com>
2025-06-17 02:05:32 +0530

    commited

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,3 +1,14 @@
+// This file configures Axios to use a base URL depending on the environment.
+// It sets the base URL to 'http://localhost:5000' in development mode
+// and to the root path '/' in production mode.
+// It is used to make HTTP requests to the backend server.
+// Importing axios library to configure HTTP requests
+// Importing axios library to configure HTTP requests
+// Importing axios library to configure HTTP requests
 import axios from 'axios';
+
 axios.defaults.baseURL =
+
   process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : '/';
+
+  // 
